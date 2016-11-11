2d5578edd4f82d89f2c6d1b3fc1640d51dc25458
synsoftech <synsoftech@synsoftech-H61M-DS2>
2016-11-11 22:18:12 +0800

    logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
         <!-- Custom stylesheet - for your changes-->
         <link rel="stylesheet" href="../css/countdown/custom.css">
         <!-- Favicon-->
-         <link rel="shortcut icon" type="image/png" href="../src/img/system/city_planning.ico" />
+         <link rel="shortcut icon" type="image/png" href="../src/img/system/profiles.png" />
         <!-- Tweaks for older IEs--><!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
@@ -31,7 +31,7 @@
             <div class="container">
                 <br><br><br><br>
               
-                <p class="social"> <img src="../src/img/system/city_planning.png" style="width: 50px;height: 50px;color: white;" ><br>
+                <p class="social"> <img src="../src/img/system/profiles.png" style="width: 50px;height: 50px;color: white;" ><br>
                     <label style="font-size: 30px;color: whitesmoke;">Profiles</label>
                 </p>
                 
