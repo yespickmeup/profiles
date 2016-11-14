7e52b404ce38d040b28cd6e98a6ecb702ca54728
synsoftech <synsoftech@synsoftech-H61M-DS2>
2016-11-14 12:20:10 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-<<<<<<< HEAD
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index');
-=======
->>>>>>> 7be84a8ad8062f342f037ce91a9727d0aca145be
+
+
